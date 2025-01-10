d367c83d182f94bc8c481c309ec3c6db653913fa
Khurramshahzad175 <khurramshahzad2440@gmail.com>
2025-01-10 07:54:24 +0500

    Added core policy

--- _02_Fast_api_with_Postgresql/__init__.py
+++ _02_Fast_api_with_Postgresql/__init__.py
@@ -1,11 +1,11 @@
-# we import FASTAPI from fastapi
-from fastapi import FastAPI , Depends ,HTTPException,status
+from fastapi import FastAPI, Depends, HTTPException, status
 from sqlmodel import Session, select
 from contextlib import asynccontextmanager
 from _02_Fast_api_with_Postgresql.db import init_db, db_session
 from _02_Fast_api_with_Postgresql.models import Student
 from _02_Fast_api_with_Postgresql.config import settings
 from sqlalchemy.exc import IntegrityError
+from starlette.middleware.cors import CORSMiddleware
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -18,12 +18,6 @@ async def lifespan(app: FastAPI):
     yield
     print("Lifespan End")
 
-# promise
-# promise in async programming has three stages
-# pending,  resolved , rejected
-
-
-# Initialize the fastapi instance to create a server
 app = FastAPI(
     title=settings.TITLE,
     description=settings.DESCRIPTION,
@@ -31,118 +25,91 @@ app = FastAPI(
     lifespan=lifespan
 )
 
-# http methods mostly used
-# get, post , put, patch, delete
+origins = [
+    "http://localhost:3000",
+    "http://localhost:3001",
+]
 
-# Root Get Route to check whether our api is up or down.
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/")
 def main_root():
     return {"message": "The API is up and running"}
 
-
 @app.get("/test-db")
-async def test_db(session:Session = Depends(db_session)):
+async def test_db(session: Session = Depends(db_session)):
     print(session)
     return {"message": "Database is connected"}
 
-# create a student post endpoint to create a new student
-@app.post("/students",status_code=status.HTTP_201_CREATED)
-async def create_student(student_data:Student, session: Session = Depends(db_session)):
+@app.post("/students", status_code=status.HTTP_201_CREATED)
+async def create_student(student_data: Student, session: Session = Depends(db_session)):
     try:
-        # Accept data from frontend in function as student_data ,, then assign student_data
-        # to data variable
         data = student_data
-
         statement = select(Student).where(Student.email == data.email)
         isEmailAlreadyExist = session.exec(statement)
 
         if isEmailAlreadyExist:
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Email already exists")
-        # here , we add our data into session
+
         session.add(data)
-        # commit the data to the database table
         session.commit()
-        
-
-        # here we return the created student data from database
         session.refresh(data)
-        print(data)
 
-
-        return {"status":True ,"message": "Student created successfully", "student_data":data}
+        return {"status": True, "message": "Student created successfully", "student_data": data}
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
-        
 
-# get all students from database and send t0 the client
-@app.get("/students/{student_id}",status_code=status.HTTP_200_OK) #dynamic path parameters
-async def get_all_students(student_id : str ,session: Session = Depends(db_session)):
+@app.get("/students", status_code=status.HTTP_200_OK)
+async def get_all_students(session: Session = Depends(db_session)):
+    students = session.exec(select(Student)).all()
 
-        statement = select(Student).where(Student.student_id == student_id)
-        isStudentExist = session.exec(statement).first() #when it finds the id at 55 from total 100 it will reutrn this not to check the 45 more
+    if not students:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Students Not found")
+    
+    return {"status": True, "message": "Students fetched successfully", "student_data": students}
 
-        if not isStudentExist:
-            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Student Not found")
-        
-        else:
-            return {"status":True ,"message": "Students fetched successfully","student_data":isStudentExist}
-        
-        
-
-# create a put request to update the student data in databae
-# fro this purpose we need two types of data
-#  1 . student id to check whether the student exsit or not
-# 2. data that we want ot change in our data base
-
-@app.put("/students/{student_id}")
-async def update_single_student(student_id : str , student_data : Student , session : Session = Depends(db_session)):
-
-    #validate data
-    student_id = student_id
-
-    if student_id is None:
-        raise HTTPException(status_code=422, detail="Student id is required.")
+@app.put("/students/{student_id}", status_code=status.HTTP_200_OK)
+async def update_single_student(student_id: str, student_data: Student, session: Session = Depends(db_session)):
     statement = select(Student).where(Student.student_id == student_id)
-    student = session.exec(statement).first() #when it finds the id at 55 from total 100 it will reutrn this not to check the 45 more
+    student = session.exec(statement).first()
 
-    if  student is None:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Student Not found")
-        
-    if student:
-         student.first_name = student_data.first_name or student.first_name   # here we are handling that in changing when these fields are empty consider the old data and add in db
-         student.last_name = student_data.last_name  or student.last_name
-         student.father_name = student_data.father_name or student.father_name
-         student.email = student_data.email or student.email                     
-         student.phone = student_data.phone or student.phone
-         student.grade  = student_data.grade or student.grade
-         student.date_of_birth = student_data.date_of_birth or student.date_of_birth
-         student.gender = student_data.gender or student.gender
+    if student is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Student Not found")
 
-    try:   
+    student.first_name = student_data.first_name or student.first_name
+    student.last_name = student_data.last_name or student.last_name
+    student.father_name = student_data.father_name or student.father_name
+    student.email = student_data.email or student.email
+    student.phone = student_data.phone or student.phone
+    student.grade = student_data.grade or student.grade
+    student.date_of_birth = student_data.date_of_birth or student.date_of_birth
+    student.gender = student_data.gender or student.gender
+
+    try:
         session.add(student)
         session.commit()
         session.refresh(student)
-
     except IntegrityError as e:
         session.rollback()
         raise HTTPException(status_code=500, detail="Database error occurred.")
 
-    return{"status":True,"message":"student updated successfully","student_data":student}
+    return {"status": True, "message": "Student updated successfully", "student_data": student}
 
-        
-# delete route to delete studetn on the basis of id in the data base
-@app.delete("/students/{student_id}")
-async def delete_student(student_id : str , session : Session = Depends(db_session)):
-     
-     if student_id is None:
-          raise HTTPException(status_code=422, detail="Student id is required.")
-     select_student = select(Student).where(Student.student_id == student_id)
-     student = session.exec(select_student).first()
-    
-     if student:
-          session.delete(student)
-          session.commit()
-          return {"status":True,"message":"student deleted successfully", "student_id":student.student_id}
-     else:
-          
+@app.delete("/students/{student_id}", status_code=status.HTTP_200_OK)
+async def delete_student(student_id: int, session: Session = Depends(db_session)):
+    statement = select(Student).where(Student.student_id == student_id)
+    student = session.exec(statement).first()
+
+    if not student:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Student Not found")
+    
+    session.delete(student)
+    session.commit()
+
+    return {"status": True, "message": "Student deleted successfully", "student_id": student.student_id}
