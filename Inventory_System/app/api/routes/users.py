801feb05951b52cd602745461ee4aad77e702259
Khurramshahzad175 <khurramshahzad2440@gmail.com>
2025-01-16 22:07:36 +0500

    " Added Signup and Signin Functionality using custon functions and JWT tokens"

--- Inventory_System/app/api/routes/users.py
+++ Inventory_System/app/api/routes/users.py
@@ -1,38 +1,85 @@
-from fastapi import APIRouter, status ,Depends , HTTPException
+from fastapi import APIRouter, Form, Response, status ,Depends , HTTPException
 from app.core.db import db_session
 from app.models.users import User
 from sqlmodel import Session, select
+from app.api.utils.user_auth_utils import  Auth
 
 user_router = APIRouter(prefix="/users",tags=["users"])
 
-@user_router.post("",status_code=status.HTTP_201_CREATED)
-async def create_new_users(user_data: User, session:Session = Depends(db_session)):
+def get_user_auth(db_session: Session = Depends(db_session)) -> Auth:
+    return Auth(db_session)
+
+
+
+@user_router.post("/signup",status_code=status.HTTP_201_CREATED)
+async def create_new_users(user_data: User, user_Auth:Auth = Depends(get_user_auth)):
 
     if not user_data.first_name  or not user_data.last_name  or not user_data.password or not user_data.email or not user_data.user_name:
         raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,detail="Please fill all fields")
 
     # check if user already exists
-    statement = select(User).where(User.user_name == user_data.user_name)
-    is_user_name_exist = session.exec(statement).first()  #return True or False
-    if is_user_name_exist:
+    user_by_user_name = await user_Auth.get_user_by_user_name(user_data.user_name)
+    if user_by_user_name:
         raise HTTPException(status_code=status.HTTP_409_CONFLICT,detail="User name already used")
     
-    # check if email already exists
-    statement = select(User).where(User.email == user_data.email)
-    is_email_already_exist = session.exec(statement).first()  #return True or False
-    if is_email_already_exist:
+    # Email validation through get_user_by_email method
+    user =  await user_Auth.get_user_by_email(user_data.email)
+    
+    if user:
         raise HTTPException(status_code=status.HTTP_409_CONFLICT,detail="Email already used")  # HTTP_409_CONFLICT Error means that the data already exist
+    print(f"user common password is {user_data.password}")
+    #Hashed Password
+    hashed_password = user_Auth.hash_password(user_data.password)
+    user_data.password = hashed_password  # replacing the original user password with hashed password
+    
+    print(f"user with hashed password is {user_data}")
 
     data= user_data
 
     try:
-        session.add(data)
-        session.commit()
-        session.refresh(data)
+        user_Auth.db_session.add(data)
+        user_Auth.db_session.commit()
+        user_Auth.db_session.refresh(data)
         return {"status":True, "message": "User created successfully", "user": data}
     except Exception as e:
         print(f"error - {e}")
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="internal server error")
+
+@user_router.post("/signin") 
+async def signin_user(response:Response,user_email: str = Form(...),user_password: str = Form(...),
+                       user_Auth:Auth = Depends(get_user_auth)):  #Query parameters, these are shown in URL
+
+    is_user_exist = await user_Auth.get_user_by_email(user_email)
+
+    if not  is_user_exist:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail = "user with this email not exist")
+    
+    is_password_match = user_Auth.verify_password(user_password,is_user_exist.password)
+    
+    if not is_password_match:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail = "Invalid Credentials")
+    
+    user_id = str(is_user_exist.id)
+    access_token , refresh_token= user_Auth.create_tokens(user_id)
+  
+    response.set_cookie(
+        key="inventory_refresh_token",
+        value= refresh_token,
+        httponly = False,
+        secure = True,
+        samesite= "strict"
+
+    )
+    return{
+        "status":True,
+        "message": "you are logged in successfully",
+        "access_token":access_token,
+        "user": is_user_exist
+
+    }
+
+# path_parameters -> static & dynamic
+# query parameters -> that we declare in function as parameter
 
 
 
---
+++ Inventory_System/app/api/utils/user_auth_utils.py
@@ -0,0 +1,111 @@
+from fastapi import Depends
+import jwt
+from sqlmodel import Session , select
+from app.core.db import db_session
+from typing import Union
+from app.models.users import User
+import bcrypt
+from bcrypt import hashpw
+from datetime import datetime, timedelta, timezone
+
+class Auth:
+    #constant
+    access_token_key = "hd23uodg97ekhbcoue"
+    refresh_token_key = "queduegdeuqdh248hdbq"
+    def __init__(self, db_session:Session = Depends(db_session)):
+        self.db_session = db_session
+
+    async def get_user_by_email(self, email:str) -> Union[User,None]:
+        # Transformation of specific data to run validations
+        # transformation is type casting , Data operations 
+        #khurramshahzad2440@gmail.com
+        email_in_lowercase = email.lower()
+        statement = select(User).where(User.email == email_in_lowercase)
+        # return true or false but it depends on retuning data
+        # {id, first_name, last_name, password,email}  
+        # True when above data is returned
+        # False when no data is returned {}
+
+        user = self.db_session.exec(statement).first()
+        # either user will be or not
+        return user
+    
+    
+    async def get_user_by_id(self, id:str) -> Union[User,None]:
+        user_id = id
+        statement = select(User).where(User.email == user_id)
+        # return true or false but it depends on retuning data
+        # {id, first_name, last_name, password,email}  
+        # True when above data is returned
+        # False when no data is returned {}
+
+        user = self.db_session.exec(statement).first()
+        # either user will be or not
+        return user
+    
+    async def get_user_by_user_name(self, user_name: str) -> Union[User, None]:
+            statement = select(User).where(User.user_name == user_name)
+            user = self.db_session.exec(statement).first()  #return True or False
+            return user
+
+    # Static method
+    @staticmethod
+    def hash_password(password:str)-> str:
+        # use bcrypt to hash password
+
+        salt = bcrypt.gensalt(10)
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+        return hashed_password.decode("utf-8")
+
+    # static methods are independent of class attributes or methods
+    @staticmethod
+    def verify_password(password: str, hashed_password: str) -> bool:
+        try:
+            if isinstance(hashed_password, str):
+                hashed_password = hashed_password.encode("utf-8")
+            return bcrypt.checkpw(password.encode("utf-8"), hashed_password)
+        except ValueError:
+        # Log the error or handle it appropriately
+            print("Invalid hashed password format.")
+            return False
+
+
+    
+    #Assumptions
+    # programmers or developer must write assumptions before writing code
+    # create tokens
+    # access_token, refresh_token
+    # user_id
+    #create access_token and refresh_token with JWT
+    # expiry date
+    # issue date
+    # return two things access token and refresh token tuple
+
+    def create_tokens(self, user_id: str)-> tuple[str, str]:
+         # issue date 
+         issued_at = datetime.now(timezone.utc)
+         # expiry date
+         access_token_expires_at = datetime.now(timezone.utc) + timedelta(hours = 3)
+         refresh_token_expires_at = datetime.now(timezone.utc) + timedelta(days = 7)
+         # create access token
+         access_token = jwt.encode(
+              {"sub": user_id, "exp": access_token_expires_at,"iat":issued_at},
+              self.access_token_key,
+              algorithm="HS256"
+              
+         )
+         # create refresh token
+         refresh_token = jwt.encode(
+              {"sub": user_id, "exp": refresh_token_expires_at,"iat":issued_at},
+              self.refresh_token_key,
+              algorithm="HS256"
+              
+         )
+
+         return access_token, refresh_token
+
+         
+#userAuth = Auth() #constructor function
+
+# is class is callable/invoke = yes
+# object itself is callable/invoke = no
