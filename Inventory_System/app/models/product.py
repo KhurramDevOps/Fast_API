c89b00677f7980a47daf96e6dbee720054849b2f
Khurramshahzad175 <khurramshahzad2440@gmail.com>
2025-01-27 00:49:30 +0500

    add relationship  between products and users

--- Inventory_System/app/models/product.py
+++ Inventory_System/app/models/product.py
@@ -1,21 +1,19 @@
-import uuid
 from sqlmodel import Field, Relationship
-from typing import Optional
-from uuid import UUID
-
 from app.models.common import BaseModel
+from typing import Optional
+import uuid
 
 class Product(BaseModel, table=True):
     __tablename__ = "products"
-    id: Optional[int] = Field(default=None, primary_key=True)
     sku_number: int
     product_name: str
-    product_description: str
+    description: str
     product_category: Optional[str]
-    product_price: float
-    product_quantity: int
+    price: float
+    quantity: int
     status: Optional[str] = Field(default="active")
-
-    # Foreign key and relationship
-    user_id: uuid.UUID = Field(default=None, foreign_key="users.id")
-    user: Optional[User] = Relationship(back_populates="products")
+    
+    user_id: uuid.UUID = Field(foreign_key="users.id")
+    
+    # Relationship to User, deferred import
+    user: "User" = Relationship(back_populates="products") # type: ignore
--- Inventory_System/app/models/user.py
+++ Inventory_System/app/models/user.py
@@ -1,18 +1,17 @@
 
 from sqlmodel import Field, Relationship
-from typing import List, Optional
-
+from typing import Optional, List
 from app.models.common import BaseModel
+from app.models.product import Product
 
 
 class User(BaseModel, table=True):
     __tablename__ = "users"
-    id: Optional[int] = Field(default=None, primary_key=True)
     first_name: str
     last_name: str
     user_name: str = Field(unique=True, index=True)
     email: str = Field(unique=True, index=True)
     password: str
-
-    # Relationship to products
+    
+    # Relationship to Product
     products: List["Product"] = Relationship(back_populates="user")
